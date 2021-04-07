fc696ec61e18e89d2f3c11e5c386ed9dc8a95432
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-04-07 16:05:21 +0200

    data14-23

--- back-end/data.js
+++ back-end/data.js
@@ -1,1 +1,84 @@
-const women = [{ name: "Sandra", born: "1992", profession: "astronaut" }];
+const women = [ {
+    id: 14, 
+    name:"Maya Angelou" ,
+    title:"Afro American human Rights activist" ,
+    contribution: "I Know Why the Caged Bird Sings",
+    year: "1928 – 2014",
+    nationality:"USA"
+    image:url()
+
+    {id: 15, 
+    name:"Ada Lovelace " ,
+    title:"World’s first computer programmer." ,
+    contribution: "That brain of mine is something more than merely mortal, as time will show",
+    year: "1815 – 1852",
+    nationality: "UK", 
+    image: ,}
+
+    {id: 16, 
+    name:"Amelia Earhart" ,
+    title:"First Women aviator" ,
+    contribution: "“Women must try to do things as men have tried. When they fail, their failure must be but a challenge to others.”",
+    year: "1897 – 1939",
+    nationality: "USA", 
+    image: ,}
+
+    {id: 17, 
+    name:"Rosalind Franklin" ,
+    title:"Doctor" ,
+    contribution: "research associate at King’s College in London and became notable for her work on X-ray diffraction images of DNA which would eventually lead to the discovery of the DNA double helix",
+    year: "1928 – 2014",
+    nationality: "UK", 
+    image: ,}
+
+    {id: 18, 
+    name:"Josephine Butler" ,
+    title:"Human Rights activist" ,
+    contribution: "She campaigned for women's suffrage, the right of women to better education, the end of coverture in British law, the abolition of child prostitution, and an end to human trafficking of young women and children into European prostitution.",
+    year: "1828 – 1906",
+    nationality: "UK", 
+    image: ,}
+
+    {id: 19, 
+    name:"Peg Hughes" ,
+    title:"Actress" ,
+    contribution: "is often credited as the first professional actress on the English stage on 8 December 1660",
+    year: "1645 – 1719",
+    nationality: "USA", 
+    image: ,}
+
+    {id: 20, 
+    name:"Annie Londonderry " ,
+    title:"Cyclist" ,
+    contribution: "first woman to cycle around the world"
+    year: "1870 – 1947",
+    nationality: "Latvia", 
+    image: ,}
+
+    {id: 21, 
+    name:"Junko Tabei" ,
+    title:"Climber" ,
+    contribution: "The first woman to scale the Everest. On May 16, 1975, .During 1990–91, Tabei reached the summit of Mt. Vinson, Antarctica's highest mountain."
+    year: "1939 – 2019",
+    nationality: "Japan", 
+    image: ,}
+
+
+    {id: 22, 
+    name:"Billie Holiday o Lady Day " ,
+    title:"Singer" ,
+    contribution: "Jazz singer and performer"
+    year: "1915 – 1959",
+    nationality: "USA", 
+    image: ,}
+
+    {id: 23, 
+    name:"Jeanne Paquin  " ,
+    title:"Fashion designer" ,
+    contribution: "was a leading French fashion designer, known for her resolutely modern and innovative designs. She was the first major female couturier and one of the pioneers of the modern fashion business"
+    year: "1869–1936",
+    nationality: "France", 
+    image: ,}
+    
+
+    }]
