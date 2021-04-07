4fb5b375a83ee3ab3cc48f07705b93c0a44ce8a7
Anna Gosme <anna.gosme@gmail.com>
2021-04-07 16:12:07 +0200

    create women objects 11 to 20

---
+++ back-end/data.js
@@ -0,0 +1,109 @@
+const women = [
+    { 
+    id: 11,
+    name: "Katherine Johnson", 
+    title: "NASA research mathematician";
+    contribution: "Katherine\'s calculations of orbital mechanics as a NASA employee were critical to the success of the first and subsequent U.S. crewed spaceflights.[1] During her 33-year career at NASA and its predecessor, she earned a reputation for mastering complex manual calculations and helped pioneer the use of computers to perform the tasks. The space agency noted her 'historical role as one of the first African-American women to work as a NASA scientist.'",
+    years: 1918-2020,
+    nationality: "American", 
+    image: url("assets/960px-Katherine_Johnson_1983.jpeg"), 
+    };
+    { 
+    id: 12,
+    name: "Radia Perlman", 
+    title: "Internet Pioneer", 
+    contribution: "Perlman invented the spanning tree algorithm and the Spanning Tree Protocol (STP), which are best explained in a poem she wrote"
+    "Algorhyme"
+    "I think that I shall never see"
+    "A graph more lovely than a tree."
+    "A tree whose crucial property"
+    "Is loop-free connectivity."
+    "A tree which must be sure to span"
+    "So packets can reach every LAN."
+    "First the root must be selected."
+    "By ID it is elected."
+    "Least cost paths from root are traced."
+    "In the tree these paths are placed."
+    "A mesh is made by folks like me"
+    "Then bridges find a spanning tree.",
+    years: 1951,
+    nationality: "American",
+    image: url("assets/440px-Radia_Perlman_2009.jpeg)",
+    };
+    { 
+    id: 13,
+    name: "Aletta Jacobs", 
+    title: "physician and women's suffrage activist", 
+    contribution: "Aletta was one of the first female physicians in the Netherlands. In 1882, she founded the world's first birth control clinic and was a leader in both the Dutch and international women's movements. She led campaigns aimed at deregulating prostitution, improving women's working conditions, promoting peace and calling for women's right to vote.",
+    years: 1854-1929,
+    nationality: "Dutch", 
+    image: url("assets/440px-Aletta_Jacobs,_1895-1905.jpeg"),
+    };
+    { 
+    id: 14, 
+    name: "Dr. Hawa Abdi", 
+    title: "Somalia’s first gynaecologist", 
+    contribution: "Affectionately known as Mama Hawa, Dr. Hawa Abdi dedicated her life to providing high-quality healthcare, education, and shelter to vulnerable people, even in times of war and famine. She was Somalia’s first female obstetrician, as well as a lawyer and humanitarian. She created the Dr. Hawa Abdi Hope Village - a community for displaced Somalis with a 400-bed hospital, primary and secondary schools, and an innovative community justice system. Recognized as one of the world's great humanitarians, Dr. Hawa was nominated for the Nobel Peace Prize and earned numerous awards, including the Roosevelt Medal and honorary doctorates from Harvard University and the University of Pennsylvania.",
+    years: 1947-2020,
+    nationality: "Somali",
+    image: url("assets/drHawaAbdi.jpeg"),
+    };
+    { 
+    id: 15,
+    name: "Reshma Saujani", 
+    title: "Education activist ", 
+    contribution:  "Reshma Saujani is the author of 'Brave, Not Perfect' and founder of Girls Who Code, the international nonprofit organization working to close the gender gap in technology and change the image of what a computer programmer looks like and does.Reshma’s TED talk, “Teach girls, bravery not perfection,” has more than four million views and has sparked a worldwide conversation about how we’re raising our girls."
+    years: 1975,
+    nationality: "American",
+    image: url("assets/RESHMA-PHOTO-1.jpeg"), 
+    };
+    { 
+    id: 16,
+    name: "Alice Catherine Evans", 
+    title: "The pioneer of safe milk", 
+    contribution: "Alice C. Evans became a researcher at the US Department of Agriculture. She investigated bacteriology in milk and cheese, demonstrating that Bacillus abortus causes the disease Brucellosis (undulant fever or Malta fever) in both cattle and humans. Her landmark discovery found acceptance despite resistance from those who were suspicious of a woman without a doctorate or medical degree.",
+    years: 1881-1975,
+    nationality: "American",
+    image: url("assets/Alice_C._Evans,_National_Photo_Company_portrait,_circa_1915.jpg")
+    };
+    { 
+    id: 17,
+    name: "Wangari Maathai", 
+    title: "Mama Trees" 
+    contribution: "' It’s the little things citizens do. That’s what will make the difference. My little thing is planting trees.' Founder of the Green Belt Movement, Wangari Maathai was called Mama Miti ,which means Mama Trees, by many people in Kenya Africa. She was the first African women to win a Nobel Peace Prize, in 2004 for her contribution to sustainable development, democracy, and peace. To this day the Green Belt Movement is planting trees and protects the 2nd largest tropical forest in the world."
+    years: 1940-2011,
+    nationality: "Kenyan",
+    image: url("assets/wangariMaathai.jpeg"),
+    };
+    { 
+    id: 18,
+    name: "Jane Goodall", 
+    title: "Internet Pioneer" 
+    contribution: "Jane Goodall is a primatologist most known for her long-term study of wild chimpanzees in Tanzania. The Gombe chimp observation, which Jane began in 1960, is the world’s longest running continuous wildlife research project.  Through her Jane Goodall Institute, she has become equally well known as a conservationist and a champion of human rights."  
+    years: 1934,
+    nationality: "British",
+    image: url("assets/janeGoodall.jpeg"),
+    };
+    { 
+    id: 19,
+    name: "Sylvia Earle", 
+    title: "Internet Pioneer" 
+    contribution: "Sylvia Earle was the first female chief scientist of the U.S. National Oceanic and Atmospheric Administration, and was named by Time Magazine as its first Hero for the Planet in 1998. She is also part of the group Ocean Elders, which is dedicated to protecting the ocean and its wildlife.",
+    years: 1935,
+    nationality: "American",
+    image: url("assets/sylviaEarle.jpeg"),
+    };
+    { 
+    id: 20,
+    name: "Adele Goldberg", 
+    title: "Co-creator of the Smalltalk System" 
+    contribution: "'Don’t ask whether you can do something, but how to do it.'This quote is from Adele Goldberg, one of the computer science pioneers from Xerox PARC. Along with Alan Kay, she developed Smalltalk. Smalltalk was used to prototype the WIMP (windows, icons, menus, pointers) interface at Xerox PARC, the cornerstone for today’s modern graphical user interfaces.",
+    years: 1945,
+    nationality: "American",
+    image: url("assets/Portrait_Adele_Goldberg-768x771.jpeg")};
+   
+    
+
+    
+ 
+
