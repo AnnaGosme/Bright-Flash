4e7c1bb1ffc7404f88df7786ce4862b2d3c10ac6
SilviaCabello <silviacabellom@gmail.com>
2021-04-07 16:16:04 +0200

    add pictures url

--- back-end/data.js
+++ back-end/data.js
@@ -5,7 +5,7 @@ const women = [
         title: "Artist, Feminist and Activist",
         contribution: "Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class, and race in Mexican society.",
         nationality: "Mexico",  
-        image: ,
+        image: url("back-end/assets/fridaKahlo.jpg"),
     }
 
     {
@@ -15,7 +15,7 @@ const women = [
         contribution: "Pioner Scientist in radioactivity. she was the first woman to win a Nobel Prize, the first and the only woman to win the Nobel Prize twice, and the only person to win the Nobel Prize in two scientific fields.",
         years: 1867-1934,
         nationality: "Poland", 
-        image: ,
+        image: url("back-end/assets/marieCurie.jpg"),
     }
 
     {
@@ -25,7 +25,7 @@ const women = [
         contribution: "She was considered one of the most important modernist 20th-century authors and also a pioneer in the use of stream of consciousness as a narrative device.",
         years: 1982-1941,
         nationality: "England", 
-        image: ,
+        image: url("back-end/assets/virginiaWoolf.jpg"),
     }
 
     {
@@ -35,7 +35,7 @@ const women = [
         contribution: "In an era when women had few opportunities to pursue artistic training or work as professional artists, Gentileschi was the first woman to become a member of the Accademia di Arte del Disegno in Florence.",
         years: 1593-1656,
         nationality: "Italy", 
-        image: ,
+        image: url("back-end/assets/artemisiaGentileschi.jpg"),
     }
 
     {
@@ -45,7 +45,7 @@ const women = [
         contribution: "She has helped inspire new, cross-generational communication about the Biafran war. She seeks to instill dignity into the finest details of each character, whether poor, middle class or rich.",
         years: 1977,
         nationality: "Nigeria", 
-        image: ,
+        image: url("back-end/assets/chimamandaNgozi.jpeg"),
     }
 
     {
@@ -55,7 +55,7 @@ const women = [
         contribution: "She was the longest-serving British prime minister of the 20th century and the first woman to hold that office.",
         years: 1920-2013,
         nationality: "England", 
-        image: ,
+        image: url("back-end/assets/margaretThatcher.jpg"),
     }
 
     {
@@ -65,7 +65,7 @@ const women = [
         contribution: "She was director of the Software Engineering Division of the MIT Instrumentation Laboratory, which developed on-board flight software for NASA's Apollo program. She founded Higher Order Software in 1976 and Hamilton Technologies in 1986",
         years: 1936,
         nationality: "US", 
-        image: ,
+        image: url("back-end/assets/margaretHamilton.jpg"),
     }
 
     {
@@ -75,7 +75,7 @@ const women = [
         contribution: "Best known for the philosophy of education that bears her name, and her writing on scientific pedagogy.Her educational method is in use today in many public and private schools globally.",
         years: 1870-1952,
         nationality: "Italy", 
-        image: ,
+        image: url("back-end/assets/mariaMontessori.jpg"),
     }
 
     {
@@ -85,7 +85,7 @@ const women = [
         contribution: "Her work was central to the understanding of the molecular structures of DNA (deoxyribonucleic acid), RNA (ribonucleic acid), viruses, coal, and graphite.",
         years: 1920-1958,
         nationality: "England", 
-        image: ,
+        image: url("back-end/assets/rosalindFranklind.jpg"),
     }
 
     {
@@ -95,7 +95,7 @@ const women = [
         contribution: "She is one of the first computer programmers. Chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.",
         years: 1815-1852,
         nationality: "England", 
-        image: ,
+        image: url("back-end/assets/adaLovelace.jpg"),
     }
 ]
 
