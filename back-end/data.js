b128091c9562cdd6595ceebb416cb7d6d25589fb
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-04-07 17:36:35 +0200

    Add data from 1 to 10

--- back-end/data.js
+++ back-end/data.js
@@ -1,4 +1,104 @@
 const women = [ 
+        { 
+        id: 1, 
+        name: "Toni Morrison",
+        title: "Writer",
+        contribution: "Toni Morrison's creative and critical effort aimed to highlight what was omitted in the history of American literature and culture. Her writing deals primarily with notion of blackness and womanhood, exposing the latent racism of the nation. Her most famous book, Beloved, gained international recognition and earned her the Nobel prize for literature in 1993." ,
+        years: 1931-2019,
+        nationality: "American", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Toni_Morrison_onbl5x.jpg",
+        },
+    
+        { 
+        id: 2, 
+        name: "Alexandria Ocasio-Cortez",
+        title: "Politician",
+        contribution: "Growing up in New York's Bronx, politician Alexandria Ocasio-Cortez has experienced from a young age the city's income inequality, which motivated her to organise her community. She run for office in 2019, becoming the youngest woman ever to be elected in the US Congress.",
+        years: 1989,
+        nationality: "American", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Alexandria-Ocasio-Cortez_hdlrc3.jpg",
+        },
+    
+        { 
+        id: 3, 
+        name: "Rita Levi Montalcini",
+        title: "Neurobiologist",
+        contribution: "Rita Levi Montalcini won the Nobel Prize in 1986 for the discovery of the nerve growth factor (NGF), a neuropeptide that is involved in the maintenance, proliferation and survival of neurons." ,
+        years: 1909-2012,
+        nationality: "Italian", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Rita_Levi-Montalcini_bandw_eyz2pq.jpg",
+        },
+    
+        { 
+        id: 4, 
+        name: "Tsai Ing-wen",
+        title: "Politician",
+        contribution: "Tsai Ing-wen is the first women to be elected president of Taiwan, Republic of China (2016). During her mandate she endorsed LGBT rights, making Taiwan the first country in Asia to legalise same-sex marriage in 2019." ,
+        years: 1956,
+        nationality: "Taiwanese", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808749/Tsai-Ing-wen_c4eho2.png",
+        },
+    
+        { 
+        id: 5, 
+        name: "Grace Hopper",
+        title: "Computer Scientist",
+        contribution: "Grace Hopper was one of the programmers of Harvard Mark I, a general purpose electromechanical computer used during World War II. A pioneer of computer programming, Grace Hopper was the first to devise the theory of machine-independent programming languages." ,
+        years: 1906-1992,
+        nationality: "American", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Grace-Hopper_fu6xbf.jpg",
+        },
+    
+        {
+        id: 6, 
+        name: "Simone de Beauvoir",
+        title: "Writer",
+        contribution: "Simone de Beauvoir's books had a significant influence on both feminist existentialism and feminist theory. The Second Sex, in particular, which deals with woman's oppression, is considered a foundational tract of contemporary feminism.",
+        years: 1908-1986,
+        nationality: "French", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808749/Simone-de-Beauvoir_sgoszv.png",
+        },
+    
+        { 
+        id: 7, 
+        name: "Maryam Mirzakhani",
+        title: "Mathematician",
+        contribution: "A mathematician and a professor of Mathematics at Stanford University, Maryam Mirzakhani's research topics included Teichmüller theory and ergodic theory. In 2014 Mirzakhani was awarded with the Fields Medal, the most prestigious awards in mathematics, becoming the first and only woman to be honored with the award." ,
+        years: 1977-2017,
+        nationality: "Iranian", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Maryam-Mirzakhani_iqf5rg.jpg",
+        },
+    
+        {
+        id: 8, 
+        name: "Kamala Harris",
+        title: "Politician and attorney",
+        contribution: "Besides being the first woman, Kamala Harris is also the first African American and first Asian American to be elected vice president of the United States in 2020." ,
+        years: 1964,
+        nationality: "American", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Kamala-Harris_l77sjk.jpg",
+        },
+    
+        { 
+        id: 9, 
+        name: "Margherita Hack",
+        title: "Astrophysicist and scientific disseminator.",
+        contribution: "Active also in the social and political field, Margherita Hack has been the first Italian woman to administrate the Trieste Astronomical Observatory from 1964 to 1987." ,
+        years: 1923-2013,
+        nationality: "Italian", 
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617809751/Margherita-Hack_akgmoa.jpg",
+        },
+    
+        { 
+        id: 10,
+        name: "Samantha Cristoforetti",
+        title: "Pilot, astronaut and engineer",
+        contribution: "Samantha Cristoforetti is the first Italian women in space, participating to 2014's European Space Agency expedition. She is scheduled to return to the International Space Station in 2022.", 
+        years: 1977, 
+        nationality: "Italian",
+        image: "https://res.cloudinary.com/dg5lakmem/image/upload/v1617808748/Samantha-Cristoforetti_mneank.jpg", 
+    },
+
     {
         id: 34, 
         name: "Frida Kahlo",
