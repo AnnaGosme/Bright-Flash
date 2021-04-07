6e075065c16b6371530297dd179974b14f1ec1fb
SilviaCabello <silviacabellom@gmail.com>
2021-04-07 16:20:55 +0200

    change id numbers

--- back-end/data.js
+++ back-end/data.js
@@ -1,6 +1,6 @@
 const women = [ 
     {
-        id: 34, 
+        id: 31, 
         name: "Frida Kahlo",
         title: "Artist, Feminist and Activist",
         contribution: "Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class, and race in Mexican society.",
@@ -9,7 +9,7 @@ const women = [
     }
 
     {
-        id: 35, 
+        id: 32, 
         name: "Marie Curie",
         title: "Scientist",
         contribution: "Pioner Scientist in radioactivity. she was the first woman to win a Nobel Prize, the first and the only woman to win the Nobel Prize twice, and the only person to win the Nobel Prize in two scientific fields.",
@@ -19,7 +19,7 @@ const women = [
     }
 
     {
-        id: 36, 
+        id: 33, 
         name: "Virginia Woolf",
         title: "Writer",
         contribution: "She was considered one of the most important modernist 20th-century authors and also a pioneer in the use of stream of consciousness as a narrative device.",
@@ -29,7 +29,7 @@ const women = [
     }
 
     {
-        id: 37, 
+        id: 34, 
         name: "Artemisia Gentileschi",
         title: "Painter",
         contribution: "In an era when women had few opportunities to pursue artistic training or work as professional artists, Gentileschi was the first woman to become a member of the Accademia di Arte del Disegno in Florence.",
@@ -39,7 +39,7 @@ const women = [
     }
 
     {
-        id: 38, 
+        id: 35, 
         name: "Chimamanda Ngozi Adichie",
         title: "Writer",
         contribution: "She has helped inspire new, cross-generational communication about the Biafran war. She seeks to instill dignity into the finest details of each character, whether poor, middle class or rich.",
@@ -49,7 +49,7 @@ const women = [
     }
 
     {
-        id: 39, 
+        id: 36, 
         name: "Margaret Thatcher",
         title: "Prime Minister of the United Kingdom",
         contribution: "She was the longest-serving British prime minister of the 20th century and the first woman to hold that office.",
@@ -59,7 +59,7 @@ const women = [
     }
 
     {
-        id: 40, 
+        id: 37, 
         name: "Margaret Hamilton",
         title: "Computer Scientist and Systems Engineer",
         contribution: "She was director of the Software Engineering Division of the MIT Instrumentation Laboratory, which developed on-board flight software for NASA's Apollo program. She founded Higher Order Software in 1976 and Hamilton Technologies in 1986",
@@ -69,7 +69,7 @@ const women = [
     }
 
     {
-        id: 41, 
+        id: 38, 
         name: "Maria Montessori",
         title: "Physician and Educator",
         contribution: "Best known for the philosophy of education that bears her name, and her writing on scientific pedagogy.Her educational method is in use today in many public and private schools globally.",
@@ -79,7 +79,7 @@ const women = [
     }
 
     {
-        id: 42, 
+        id: 39, 
         name: "Rosalind Franklin",
         title: "Chemist",
         contribution: "Her work was central to the understanding of the molecular structures of DNA (deoxyribonucleic acid), RNA (ribonucleic acid), viruses, coal, and graphite.",
@@ -89,7 +89,7 @@ const women = [
     }
 
     {
-        id: 43, 
+        id: 40, 
         name: "Ada Lovelace",
         title: "Mathematician and Writer",
         contribution: "She is one of the first computer programmers. Chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.",
