a0bd19eb393bf0382a4fdfe412f3498b5ea31007
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-04-22 14:43:08 +0200

    Fix code in header.js to let header disappear from home. Change picture of contact from landscape to portrait.

--- front-end/src/components/ListOfWomen.js
+++ front-end/src/components/ListOfWomen.js
@@ -1,9 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import Header from "./Header/Header";
 import Footer from "../Footer";
 //import css
 
 export default function ListOfWomen() {
 	return null;
-	//fetch api of women?
 }
