6cec78f91192108c3960bc0c653ecc8ef3144fad
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-04-23 11:07:08 +0200

    Fix conflict. Footer stick on the bottom of the page on contact/about-us

--- front-end/src/components/GameIntroPage.js
+++ front-end/src/components/GameIntroPage.js
@@ -5,27 +5,27 @@ import "./GameIntroPage.css";
 // import css
 
 function GameIntroPage() {
-	return (
-	  <div>
-		<div className="selection-lev">
-		  <h5>PLEASE SELECT ONE LEVEL</h5>
-		</div>
-		<div className="mt-3">
-		  <div>
-			<button className="btn btn-one m-2">LEVEL ONE</button>
-		  </div>
-		  <div>
-			<button className="btn btn-two m-2">LEVEL TWO</button>
-		  </div>
-		  <div>
-			<button className="btn btn-three m-2">LEVEL THREE</button>
-		  </div>
-		  <div>
-			<button className="btn btn-four m-2">LEVEL FOUR</button>
-		  </div>
-		</div>
-		<div></div>
-	  </div>
-	);
-	}
-	export default GameIntroPage;
+  return (
+    <div className = "page-gameintro">
+      <div className="selection-lev">
+        <h5>PLEASE SELECT ONE LEVEL</h5>
+      </div>
+      <div className="mt-3">
+        <div>
+          <button className="btn btn-one m-2">LEVEL 1</button>
+        </div>
+        <div>
+          <button className="btn btn-two m-2">LEVEL 2</button>
+        </div>
+        <div>
+          <button className="btn btn-three m-2">LEVEL 3</button>
+        </div>
+        <div>
+          <button className="btn btn-four m-2">LEVEL 4</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+export default GameIntroPage;
