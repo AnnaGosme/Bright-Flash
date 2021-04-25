efa38a759f9ff562087722d41e8ea58168b091bd
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-04-25 08:19:55 +0200

    Add component for mobile version of homepage. Style accordingly

---
+++ front-end/src/components/Home/HomeMobile.js
@@ -0,0 +1,10 @@
+import Header from "../Header/Header";
+import "./HomeMobile.css";
+
+export default function HomeMobile() {
+	return (
+		<div className="body-mobile">
+			<img src="https://res.cloudinary.com/dg5lakmem/image/upload/v1618422713/Bright%20Flash/Women-bright-flash_ozg2mg.jpg" />
+		</div>
+	);
+}
