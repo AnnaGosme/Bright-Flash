74743aa19c3ec735d74d344365312364d7ec5b2f
Alessandra Pettinato <alessandrapettinato@Alessandras-MBP.lan>
2021-04-26 17:13:23 +0200

    Fix hamb menu functionality in landingpage

--- front-end/src/components/Women-Info/ListOfWomen.js
+++ front-end/src/components/Women-Info/ListOfWomen.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Header from "./Header/Header";
-import Footer from "../Footer";
+import Header from "../Header/Header";
+import Footer from "../../Footer";
 //import css
 
 export default function ListOfWomen() {
