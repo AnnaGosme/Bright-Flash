fa8972442a029d55bcb1f902911c136b6b7f2b2a
Anna Gosme <anna.gosme@gmail.com>
2021-04-26 23:37:06 +0200

    update quoteFetch

--- front-end/src/components/Footer/FooterQuote.js
+++ front-end/src/components/Footer/FooterQuote.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function FetchQuote() {
-    const [quote,setQuote] = useState("");
-    const [loading,setLoading] = useState(true);
-    const [name,setName]= useState("");
+  const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
 
-    useEffect(() => {
-        getQuote();
-       const intervalID = setInterval(()=> {
-        getQuote()
-       }, 60 * 1000);
-    return ()=>{
-        clearInterval(intervalID);
-    }
-    },[])
+  useEffect(() => {
+    getQuote();
+    const intervalID = setInterval(() => {
+      getQuote(quote[Math.floor(Math.random() * quote.length) + 1]);
+    }, 60 * 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
 
-   function getQuote() { 
-        fetch("http://localhost:5000/")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setQuote(data.quote);
-            setName(data.name);
-      })
-   }
-            
-    return (
-        <div>
-            <p>{`${data.quote}- ${data.name}`}</p>
-        </div>
-    )
+  function getQuote() {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data.quote);
+        setName(data.name);
+      });
+  }
+
+  return (
+    <div>
+      <p>{`${data.quote}- ${data.name}`}</p>
+    </div>
+  );
 }
