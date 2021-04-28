b8fda9ad02ed520a3a478444fcad72293b23ed2a
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-04-28 14:31:31 +0200

    Change location in header

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -3,11 +3,11 @@ import * as GoIcons from "react-icons/go";
 import * as AiIcons from "react-icons/ai";
 import { MenuItems } from "./MenuItems";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useLocation,
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useLocation,
 } from "react-router-dom";
 
 import "./header.css";
@@ -19,44 +19,44 @@ import About from "../About";
 import Contact from "../Contact";
 
 export default function Header() {
-  const [menuList, setMenuList] = useState(false);
-  const showHide = () => setMenuList(!menuList);
+	const [menuList, setMenuList] = useState(false);
+	const showHide = () => setMenuList(!menuList);
 
-  let location = useLocation();
+	let location = useLocation();
 
-  return (
-    location.pathname !== "/" && (
-      <div>
-        <nav className="navbar-items">
-          <div className="navbar-logo">
-            <img
-              id="logo"
-              src="https://res.cloudinary.com/dg5lakmem/image/upload/v1618495596/Bright%20Flash/Bright-Flash-logo_vxoimo.png"
-              alt="bright-flash-logo"
-            />
-          </div>
-          <div className="menu-icon" onClick={showHide}>
-            {!menuList ? <GoIcons.GoThreeBars /> : <AiIcons.AiOutlineClose />}
-          </div>
-          <div className="menu-list">
-            <ul className={menuList ? "nav-menu active" : "nav-menu"}>
-              {MenuItems.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <Link
-                      to={item.url}
-                      className={item.cName}
-                      onClick={() => showHide(menuList)}
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </nav>
-      </div>
-    )
-  );
+	return (
+		location.pathname !== "/" && (
+			<div>
+				<nav className="navbar-items">
+					<div className="navbar-logo">
+						<img
+							id="logo"
+							src="https://res.cloudinary.com/dg5lakmem/image/upload/v1618495596/Bright%20Flash/Bright-Flash-logo_vxoimo.png"
+							alt="bright-flash-logo"
+						/>
+					</div>
+					<div className="menu-icon" onClick={showHide}>
+						{!menuList ? <GoIcons.GoThreeBars /> : <AiIcons.AiOutlineClose />}
+					</div>
+					<div className="menu-list">
+						<ul className={menuList ? "nav-menu active" : "nav-menu"}>
+							{MenuItems.map((item, index) => {
+								return (
+									<li key={index}>
+										<Link
+											to={item.url}
+											className={item.cName}
+											onClick={() => showHide(menuList)}
+										>
+											{item.title}
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</nav>
+			</div>
+		)
+	);
 }
