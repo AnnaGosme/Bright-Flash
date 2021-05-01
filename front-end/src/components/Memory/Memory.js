16d32241b389fdadc56d387a1cb25ba80c5b6d57
SilviaCabello <silviacabellom@gmail.com>
2021-05-01 21:03:27 +0200

    change background for game

--- front-end/src/components/Memory/Memory.js
+++ front-end/src/components/Memory/Memory.js
@@ -1,17 +1,16 @@
 import Card from "../Card";
 import Header from "../Header/Header";
 import Footer from "../../Footer";
+import { Link } from "react-router-dom";
 import "./Memory.css";
 import Game from "./Game";
-import {
-  useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function Memory({ data }) {
   const { level } = useParams(); // get the parameters from the router path
   return (
     <div>
-      <Game data={data} level={level}/> {/* send level as props */}
+      <Game data={data} level={level} /> {/* send level as props */}
       <br />
       <div className="button-container">
         <button
@@ -23,9 +22,11 @@ function Memory({ data }) {
           RESET
         </button>
 
-        <button className="button" id="go-on" type="button">
-          Go On
-        </button>
+        <Link to="2">
+          <button className="button" id="go-on" type="button">
+            Next
+          </button>
+        </Link>
       </div>
     </div>
   );
