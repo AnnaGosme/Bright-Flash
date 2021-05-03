db7141c1cdbede46d98430cf2f670786e2434366
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-03 18:16:15 +0200

    Fix level links in GameIntroPage. Minor changes to files in the Memory folder. Need to clean-up code of those files

--- front-end/src/components/Memory/Memory.js
+++ front-end/src/components/Memory/Memory.js
@@ -1,32 +1,32 @@
 import Card from "../Card";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+
 import "./Memory.css";
 import Game from "./Game";
 import { useParams } from "react-router-dom";
 
 function Memory({ data }) {
-  const { level } = useParams(); // get the parameters from the router path
-  return (
-    <div className="gameContainer">
-      <Game data={data} level={level} /> {/* send level as props */}
-      <br />
-      <div className="button-container">
-        <button
-          className="button"
-          id="replay"
-          type="button"
-          onClick={() => window.location.reload(false)}
-        >
-          RESET
-        </button>
+	const { level } = useParams(); // get the parameters from the router path
+	return (
+		<div>
+			<Game data={data} level={level} /> {/* send level as props */}
+			<br />
+			<div className="button-container">
+				<button
+					className="button"
+					id="replay"
+					type="button"
+					onClick={() => window.location.reload(false)}
+				>
+					RESET
+				</button>
 
-        <button className="button" id="go-on" type="button">
-          Play
-        </button>
-      </div>
-    </div>
-  );
+				<button className="button" id="go-on" type="button">
+					Go On
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Memory;
