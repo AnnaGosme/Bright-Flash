79d66804c44027a56c10c40fef18fd7ead196e41
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-03 19:46:00 +0200

    Fix in homepage, header, footer (useLocation)

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,42 +1,41 @@
 import { useState } from "react";
 import { ImageMap } from "@qiuz/react-image-map";
-import { Link } from "react-router-dom";
 
 import LandingPageMenu from "./LandingPageMenu";
 
 import "./Home.css";
 
 const HomePage = () => {
-  const [showMenu, setShowMenu] = useState(false);
+	const [showMenu, setShowMenu] = useState(false);
 
-  const img =
-    "https://res.cloudinary.com/dg5lakmem/image/upload/v1619255445/Bright%20Flash/collage-menu_bnx0n7.jpg";
-  const mapArea = [
-    {
-      width: "23.448625180897245%",
-      height: "23.615541922290383%",
-      left: "22.431259044862518%",
-      top: "4.498977505112485%",
-    },
-  ];
-  const onMapClick = () => {
-    setShowMenu(!showMenu);
-  };
+	const img =
+		"https://res.cloudinary.com/dg5lakmem/image/upload/v1619255445/Bright%20Flash/collage-menu_bnx0n7.jpg";
+	const mapArea = [
+		{
+			width: "23.448625180897245%",
+			height: "23.615541922290383%",
+			left: "22.431259044862518%",
+			top: "4.498977505112485%",
+		},
+	];
+	const onMapClick = () => {
+		setShowMenu(!showMenu);
+	};
 
-  return (
-    <div>
-      <div className="body-cover">
-        {showMenu ? <LandingPageMenu /> : ""}
+	return (
+		<div>
+			<div className="body-cover">
+				{showMenu ? <LandingPageMenu /> : ""}
 
-        <ImageMap
-          className="collage-click"
-          src={img}
-          map={mapArea}
-          onMapClick={onMapClick}
-        />
-      </div>
+				<ImageMap
+					className="collage-click"
+					src={img}
+					map={mapArea}
+					onMapClick={onMapClick}
+				/>
+			</div>
 
-      {/* MOBILE VERSION. Desktop CSS all the display are = none. They are activated again in mediaquery. */}
+			{/* MOBILE VERSION. Desktop CSS all the display are = none. They are activated again in mediaquery. */}
 
 			<div>
 				<img
