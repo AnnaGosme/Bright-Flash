1cfe356abf9656407e4659bf1768482704fb6d33
Anna Gosme <anna.gosme@gmail.com>
2021-05-04 15:52:08 +0200

    desktop version complete

--- front-end/src/components/GameIntroPage.js
+++ front-end/src/components/GameIntroPage.js
@@ -4,7 +4,17 @@ import "./GameIntroPage.css";
 function GameIntroPage() {
   return (
     <div className="page-wrapper">
-      <h5 className="level-select-text">PLEASE SELECT A LEVEL</h5>
+      <span className="game-rules">
+        Learn about some of the women who have made and are still making
+        significant contributions to society through a game of memory. Click on
+        a first card, then try to find the card’s match. The counter will keep
+        track of how many turns to take to find all the matches. Progress up to
+        the highest level by clicking the play next button.<br></br> When you’ve
+        completed all the levels, why don’t you take a look at the Women page,
+        and learn about the stories behind the faces that are now so familiar to
+        you?
+      </span>
+      {/* <h5 className="level-select-text">PLEASE SELECT A LEVEL</h5> */}
 
       <div className="level-select-container">
         <Link to="/memory/1">
@@ -22,10 +32,8 @@ function GameIntroPage() {
         <Link to="/memory/4">
           <button className="level-button">LEVEL 4</button>
         </Link>
-
       </div>
-      </div>
-    
+    </div>
   );
 }
 export default GameIntroPage;
